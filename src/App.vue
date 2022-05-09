e8705fc7f2d61fe5997f4a6d0aa0559fd8cd9cc8
mahmudhaisan <mahmudhaisan@gmail.com>
2022-05-09 19:00:54 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Message is : {{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello world",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import NewApp from './App.vue';
+
+
+const app = createApp(NewApp);
+
+app.mount("#app");
